8d2bbec52c2e78057d08bb8d51bd75de3833e133
ESOLUTIONS\Lokesh.G <lokesh.g@trivium-esolutions.com>
2019-11-18 15:19:16 +0530

    Pom Changesddd

--- src/main/java/com/movies/util/IpAddressUtil.java
+++ src/main/java/com/movies/util/IpAddressUtil.java
@@ -2,7 +2,6 @@ package com.movies.util;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
 
@@ -12,10 +11,7 @@ public class IpAddressUtil {
         String systemipaddress = ""; 
         try
         { 
-        	InetAddress localhost = InetAddress.getLocalHost(); 
-            System.out.println("System IP Address : " + 
-                          (localhost.getHostAddress()).trim()); 
-            URL url_name = new URL("http://bot.whatismyipaddress.com"); 
+            URL url_name = new URL("https://ipapi.co/json/"); 
             BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream())); 
             systemipaddress = sc.readLine().trim(); 
         } 
