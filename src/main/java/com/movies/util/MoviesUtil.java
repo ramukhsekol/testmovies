859a097bec9df3fd2660b9b27fe0bf8fbe34f07e
ESOLUTIONS\Lokesh.G <lokesh.g@trivium-esolutions.com>
2019-11-26 15:29:21 +0530

    added new movies

--- src/main/java/com/movies/util/MoviesUtil.java
+++ src/main/java/com/movies/util/MoviesUtil.java
@@ -9,6 +9,13 @@ public class MoviesUtil {
 	static {
 		movies.put("tt0924317", "https://gounlimited.to/embed-oiv511tmeqj9.html");
 		movies.put("tt6836936", "https://gounlimited.to/embed-pp06jwzhwdqk.html");
+		movies.put("tt0374497", "https://www.youtube.com/embed/52QtjUD5g74");
+		movies.put("tt0858492", "https://www.youtube.com/embed/whIpRskedkQ");
+		movies.put("tt0805492", "https://www.youtube.com/embed/a08-34byBDY");
+		movies.put("tt1783411", "https://www.youtube.com/embed/NodmtYP9P-g");
+		
+		
+		
 	}
 	
 	public static String getMovie(String key) {
